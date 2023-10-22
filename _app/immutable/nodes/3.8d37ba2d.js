f1624976ad018ef2840e159ccbca3608313abc69
Parker Snell <parkersnell@gmail.com>
2023-10-22 11:59:01 -0700

    Updates

---
+++ _app/immutable/nodes/3.8d37ba2d.js
@@ -0,0 +1,47 @@
+import{S as $e,i as Pe,s as Se,a as C,y as xe,k as l,q as a,M as Ce,h as n,c as j,z as je,l as i,m as h,r,n as t,C as qe,p as Be,b as J,A as Ge,D as e,g as He,d as Me,B as ze}from"../chunks/index.908e4dac.js";import{T as Je}from"../chunks/Title.a1197c98.js";function Te(T){let d;return{c(){d=a("About")},l(c){d=r(c,"About")},m(c,q){J(c,d,q)},d(c){c&&n(d)}}}function Ue(T){let d,c,q,s,w,fe,U,g,W,_,D,F,v,L,Q,k,V,Z,b,O,K,N,f,R,I,X,Y,A,ee,te,ae,m,re,E,ne,oe,$,se,le,ie,y,he,P,ue,ce,S,ge,de,pe,M,z;return c=new Je({props:{$$slots:{default:[Te]},$$scope:{ctx:T}}}),{c(){d=C(),xe(c.$$.fragment),q=C(),s=l("div"),w=l("img"),U=C(),g=l("p"),W=a(`I started writing software in 2002 with a small hobbyist language called
+    IBasic that no longer exists. Soon after this I started writing small
+    websites in PHP/MySQL and games in Java. After publishing
+    an `),_=l("a"),D=a("Android game"),F=a(`
+    (no longer on Google Play), I learned C++ by writing an
+    `),v=l("a"),L=a(`IBasic-compatible
+      compiler`),Q=a(`. I continued working in C++ and JavaScript, completing a
+    `),k=l("a"),V=a("truss shipment scheduler"),Z=a(`
+    and a `),b=l("a"),O=a("small tanks game"),K=a(`
+    among many other projects.`),N=C(),f=l("p"),R=a("In 2017 I started learning the "),I=l("a"),X=a("Unity"),Y=a(`
+    game engine and created a
+    `),A=l("a"),ee=a("Smash-style phone game"),te=a(`
+    which sadly never got professional quality graphics and didn't make it to
+    the app stores. Additionally Unity has since moved away from the networking
+    platform I used, making the future of the game lie in a rewrite.`),ae=C(),m=l("p"),re=a(`At some point along the way, I created a rough
+    `),E=l("a"),ne=a(`proof-of-concept sudo
+      utility`),oe=a(` for Windows which has gained some minor popularity on GitHub.
+    With this project, I mainly wanted to show that such a thing was possible,
+    while recognizing that to create a production-ready security focused
+    utility would be a huge undertaking. Fortunately, someone else has
+    `),$=l("a"),se=a("picked up the slack"),le=a("."),ie=C(),y=l("p"),he=a(`In the past I have been an avid traveler; I currently live in Tucson,
+    AZ, home of the
+    `),P=l("a"),ue=a("saguaro"),ce=a(`, where
+    `),S=l("a"),ge=a("everything sunny all the time always"),de=a("."),pe=C(),M=l("div"),this.h()},l(o){Ce("svelte-1dc3lid",document.head).forEach(n),d=j(o),je(c.$$.fragment,o),q=j(o),s=i(o,"DIV",{id:!0});var u=h(s);w=i(u,"IMG",{id:!0,src:!0,alt:!0,class:!0}),U=j(u),g=i(u,"P",{});var p=h(g);W=r(p,`I started writing software in 2002 with a small hobbyist language called
+    IBasic that no longer exists. Soon after this I started writing small
+    websites in PHP/MySQL and games in Java. After publishing
+    an `),_=i(p,"A",{href:!0,target:!0,rel:!0});var me=h(_);D=r(me,"Android game"),me.forEach(n),F=r(p,`
+    (no longer on Google Play), I learned C++ by writing an
+    `),v=i(p,"A",{href:!0,target:!0,rel:!0});var ye=h(v);L=r(ye,`IBasic-compatible
+      compiler`),ye.forEach(n),Q=r(p,`. I continued working in C++ and JavaScript, completing a
+    `),k=i(p,"A",{href:!0,target:!0,rel:!0});var we=h(k);V=r(we,"truss shipment scheduler"),we.forEach(n),Z=r(p,`
+    and a `),b=i(p,"A",{href:!0,target:!0,rel:!0});var _e=h(b);O=r(_e,"small tanks game"),_e.forEach(n),K=r(p,`
+    among many other projects.`),p.forEach(n),N=j(u),f=i(u,"P",{});var B=h(f);R=r(B,"In 2017 I started learning the "),I=i(B,"A",{href:!0,target:!0,rel:!0});var ve=h(I);X=r(ve,"Unity"),ve.forEach(n),Y=r(B,`
+    game engine and created a
+    `),A=i(B,"A",{href:!0,target:!0,rel:!0});var ke=h(A);ee=r(ke,"Smash-style phone game"),ke.forEach(n),te=r(B,`
+    which sadly never got professional quality graphics and didn't make it to
+    the app stores. Additionally Unity has since moved away from the networking
+    platform I used, making the future of the game lie in a rewrite.`),B.forEach(n),ae=j(u),m=i(u,"P",{});var G=h(m);re=r(G,`At some point along the way, I created a rough
+    `),E=i(G,"A",{href:!0,target:!0,rel:!0});var be=h(E);ne=r(be,`proof-of-concept sudo
+      utility`),be.forEach(n),oe=r(G,` for Windows which has gained some minor popularity on GitHub.
+    With this project, I mainly wanted to show that such a thing was possible,
+    while recognizing that to create a production-ready security focused
+    utility would be a huge undertaking. Fortunately, someone else has
+    `),$=i(G,"A",{href:!0,target:!0,rel:!0});var Ie=h($);se=r(Ie,"picked up the slack"),Ie.forEach(n),le=r(G,"."),G.forEach(n),ie=j(u),y=i(u,"P",{});var H=h(y);he=r(H,`In the past I have been an avid traveler; I currently live in Tucson,
+    AZ, home of the
+    `),P=i(H,"A",{href:!0,target:!0,rel:!0});var Ae=h(P);ue=r(Ae,"saguaro"),Ae.forEach(n),ce=r(H,`, where
+    `),S=i(H,"A",{href:!0,target:!0,rel:!0});var Ee=h(S);ge=r(Ee,"everything sunny all the time always"),Ee.forEach(n),de=r(H,"."),H.forEach(n),pe=j(u),M=i(u,"DIV",{style:!0}),h(M).forEach(n),u.forEach(n),this.h()},h(){document.title="parkovski - about",t(w,"id","saguaro"),qe(w.src,fe="/saguaro.jpg")||t(w,"src",fe),t(w,"alt","Saguaro flowers"),t(w,"class","svelte-lwch8l"),t(_,"href","https://github.com/parkovski/Chaturanga"),t(_,"target","_blank"),t(_,"rel","noreferrer"),t(v,"href","https://github.com/parkovski/glazc"),t(v,"target","_blank"),t(v,"rel","noreferrer"),t(k,"href","https://github.com/parkovski/tsched"),t(k,"target","_blank"),t(k,"rel","noreferrer"),t(b,"href","https://github.com/parkovski/tanks"),t(b,"target","_blank"),t(b,"rel","noreferrer"),t(I,"href","https://www.unity.com"),t(I,"target","_blank"),t(I,"rel","noreferrer"),t(A,"href","https://github.com/parkovski/scifi"),t(A,"target","_blank"),t(A,"rel","noreferrer"),t(E,"href","https://github.com/parkovski/wsudo"),t(E,"target","_blank"),t(E,"rel","noreferrer"),t($,"href","https://github.com/gerardog/gsudo"),t($,"target","_blank"),t($,"rel","noreferrer"),t(P,"href","https://en.wikipedia.org/wiki/Saguaro"),t(P,"target","_blank"),t(P,"rel","noreferrer"),t(S,"href","https://www.youtube.com/watch?v=OhohteHuyPM"),t(S,"target","_blank"),t(S,"rel","noreferrer"),Be(M,"clear","right"),t(s,"id","wrap-body")},m(o,x){J(o,d,x),Ge(c,o,x),J(o,q,x),J(o,s,x),e(s,w),e(s,U),e(s,g),e(g,W),e(g,_),e(_,D),e(g,F),e(g,v),e(v,L),e(g,Q),e(g,k),e(k,V),e(g,Z),e(g,b),e(b,O),e(g,K),e(s,N),e(s,f),e(f,R),e(f,I),e(I,X),e(f,Y),e(f,A),e(A,ee),e(f,te),e(s,ae),e(s,m),e(m,re),e(m,E),e(E,ne),e(m,oe),e(m,$),e($,se),e(m,le),e(s,ie),e(s,y),e(y,he),e(y,P),e(P,ue),e(y,ce),e(y,S),e(S,ge),e(y,de),e(s,pe),e(s,M),z=!0},p(o,[x]){const u={};x&1&&(u.$$scope={dirty:x,ctx:o}),c.$set(u)},i(o){z||(He(c.$$.fragment,o),z=!0)},o(o){Me(c.$$.fragment,o),z=!1},d(o){o&&n(d),ze(c,o),o&&n(q),o&&n(s)}}}class Fe extends $e{constructor(d){super(),Pe(this,d,null,Ue,Se,{})}}export{Fe as component};
